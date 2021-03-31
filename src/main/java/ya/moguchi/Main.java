504884fd0b715bdbd478fbedd6141e00d0c7e4bc
Evgeniy Gryaznov <e.gryaznov@x5.ru>
2021-03-31 12:32:27 +0300

    rearranged args

--- src/main/java/ya/moguchi/Main.java
+++ src/main/java/ya/moguchi/Main.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 import ya.moguchi.internal.Pattern;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 /**
  * TODO:
@@ -25,26 +24,23 @@ public class Main
         System.out.println("Megexps v0.1, Enter command:");
         final var s = new Scanner(System.in);
         var command = "initial";
-        final Stack<String> history = new Stack<>();
         do {
-            if (process(command, history))
-                history.add(command);
+            process(command);
             System.out.print("> ");
             command = s.nextLine();
         }
         while (!command.equalsIgnoreCase("q"));
     }
 
-    private static boolean process(@NotNull final String command, final Stack<String> history) throws Exception
+    private static void process(@NotNull final String command) throws Exception
     {
         if (command.equals("initial"))
-            return false;
+            return;
         //
-        boolean isNewCommand = false;
         var tokens = command.split(" ");
         switch (tokens.length)
         {
-            case 4:
+            case 4: // regex x regex string
                 if (tokens[1].equalsIgnoreCase("x"))
                 {
                     final var firstPattern = ya.moguchi.internal.Compiler.compile(tokens[0]);
@@ -54,42 +50,26 @@ public class Main
                     System.out.println(intersection.matches(patient));
                 }
                 else
-                    System.out.println("Формат пересечения <megexp> X <megexp> <patient>");
+                    System.out.println("Формат пересечения <megexp> X <megexp> <string>");
                 break;
-            case 1:
-                if (tokens[0].equalsIgnoreCase("u"))
-                {
-                    if (history.isEmpty())
-                        System.out.println("История пуста");
-                    else
-                    {
-                        final String up = history.pop();
-                        System.out.printf("> %s\n", up);
-                        process(up, history);
-                    }
-                }
-                else
-                    System.out.println("Введите 2 аргумента");
+            case 3: // regex N .
+            {
+                final var compiled = ya.moguchi.internal.Compiler.compile(tokens[0]);
+                final int nOfSolutionsToPrint = Integer.parseInt(tokens[1]);
+                compiled.solutions()
+                        .limit(nOfSolutionsToPrint)
+                        .forEach(System.out::println);
                 break;
-            default:
+            }
+            default: // regex string
                 final var compiled = ya.moguchi.internal.Compiler.compile(tokens[0]);
                 final var match = tokens[1];
-                switch (match)
-                {
-                    case "!":
-                        System.out.println(compiled);
-                        break;
-                    case ".":
-                        compiled.solutions()
-                                .limit(10)
-                                .forEach(System.out::println);
-                        break;
-                    default:
-                        System.out.println(compiled.matches(tokens[1]));
+                if ("!".equals(match)) {
+                    System.out.println(compiled);
+                } else {
+                    System.out.println(compiled.matches(tokens[1]));
                 }
-                isNewCommand = true;
         }
-        return isNewCommand;
     }
 
 }
